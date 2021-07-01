3886daf7b004ca3211805a3bb65b748d5f9b7dad
Alberto <alberto.arteaga@fr3estudio.mx>
2021-06-30 23:49:18 -0500

    Cambio de rutas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <link rel="icon" href="{{ asset('assets/img/favicon.svg') }}" type="image/x-icon"/>
 
+
     <!-- Fonts and icons -->
     <script src="{{asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
     <script>
@@ -19,23 +20,23 @@
     </script>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/atlantis.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/css/atlantis.min.css')}}">
 
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link rel="stylesheet" href="assets/css/demo.css">
+    <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}">
 
 
 
 
     <!-- css chat -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
 
 
 
     <!-- CSS no -->
-    <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
-    <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/styles/core.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/styles/icon-font.min.css')}}">
 
 
 
@@ -1270,48 +1271,48 @@
 
 
 <!--   Core JS Files   -->
-<script src="assets/js/core/jquery.3.2.1.min.js"></script>
-<script src="assets/js/core/popper.min.js"></script>
-<script src="assets/js/core/bootstrap.min.js"></script>
+<script src="{{asset('assets/js/core/jquery.3.2.1.min.js')}}"></script>
+<script src="{{asset('assets/js/core/popper.min.js')}}"></script>
+<script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
 
 <!-- jQuery UI -->
-<script src="assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-<script src="assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+<script src="{{asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')}}"></script>
+<script src="{{asset('assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js')}}"></script>
 
 <!-- jQuery Scrollbar -->
-<script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<script src="{{asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
 
 
 <!-- Chart JS -->
-<script src="assets/js/plugin/chart.js/chart.min.js"></script>
+<script src="{{asset('assets/js/plugin/chart.js/chart.min.js')}}"></script>
 
 <!-- jQuery Sparkline -->
-<script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+<script src="{{asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
 
 <!-- Chart Circle -->
-<script src="assets/js/plugin/chart-circle/circles.min.js"></script>
+<script src="{{asset('assets/js/plugin/chart-circle/circles.min.js')}}"></script>
 
 <!-- Datatables -->
-<script src="assets/js/plugin/datatables/datatables.min.js"></script>
+<script src="{{asset('assets/js/plugin/datatables/datatables.min.js')}}"></script>
 
 <!-- Bootstrap Notify -->
-<script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+<script src="{{asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
 
 <!-- jQuery Vector Maps -->
-<script src="assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
-<script src="assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
+<script src="{{asset('assets/js/plugin/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('assets/js/plugin/jqvmap/maps/jquery.vmap.world.js')}}"></script>
 
 <!-- Sweet Alert -->
-<script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+<script src="{{asset('assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
 
 <!-- Atlantis JS -->
-<script src="assets/js/atlantis.min.js"></script>
+<script src="{{asset('assets/js/atlantis.min.js')}}"></script>
 
 
 
 <!-- Atlantis DEMO methods, don't include it in your project! -->
-<script src="assets/js/setting-demo.js"></script>
-<script src="assets/js/demo.js"></script>
+<script src="{{asset('assets/js/setting-demo.js')}}"></script>
+<script src="{{asset('assets/js/demo.js')}}"></script>
 <script>
     Circles.create({
         id:'circles-1',
@@ -1410,8 +1411,8 @@
 
 
 <!-- js chat -->
-<script src="vendors/scripts/core.js"></script>
-<script src="vendors/scripts/script.min.js"></script>
+<script src="{{asset('vendors/scripts/core.js')}}"></script>
+<script src="{{asset('vendors/scripts/script.min.js')}}"></script>
 
 
 
